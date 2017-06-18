1d08695b5294331503e4e26d5fdd25c765980560
Gerald Low <lowbicheng@gmail.com>
2017-06-18 07:02:28 +0100

    Gerald commitz

--- app/src/main/java/com/dji/GSDemo/GoogleMap/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/dji/GSDemo/GoogleMap/firebase/MyFirebaseMessagingService.java
@@ -1,8 +1,66 @@
 package com.dji.GSDemo.GoogleMap.firebase;
 
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.dji.GSDemo.GoogleMap.MainActivity;
+import com.dji.GSDemo.GoogleMap.R;
+
 /**
- * Created by Gerald Low on 18/6/2017.
+ * Created by Purvik Rana on 23-06-2016.
+ *
  */
 
-public class MyFirebaseMessagingService {
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //Displaying data in log
+        //It is optional
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        //Calling method to generate notification
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    //This method to generate push notification
+    private void sendNotification(String messageBody) {
+
+        //MainActivity Intent Registration
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        //Take Notification Sound
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //Generate the Notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Firebase Notification")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Create Push Notification
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 }
